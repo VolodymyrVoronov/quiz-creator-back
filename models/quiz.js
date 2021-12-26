dd68fbd902c2cf7edab68a23c1ebfeccea55cebe
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-26 15:32:40 +0100

    chore(quiz): fixed __v.

--- models/quiz.js
+++ models/quiz.js
@@ -1,28 +1,26 @@
 import mongoose from "mongoose";
 
-const quizSchema = mongoose.Schema(
-  { __v: { type: Number, select: false } },
-  {
-    id: { type: String, required: true },
-    quizTitle: { type: String, required: true },
-    userId: { type: String, required: true },
+const quizSchema = mongoose.Schema({
+  id: { type: String, required: true },
+  quizTitle: { type: String, required: true },
+  userId: { type: String, required: true },
 
-    questions: [
-      {
-        id: { type: String, required: true },
-        question: { type: String, required: true },
-        options: [
-          {
-            id: { type: String, required: true },
-            answerOption: { type: String, required: true },
-            correct: { type: Boolean, required: true },
-            userAnswer: { type: Boolean, required: true },
-          },
-        ],
-      },
-    ],
-  }
-);
+  questions: [
+    {
+      id: { type: String, required: true },
+      question: { type: String, required: true },
+      options: [
+        {
+          id: { type: String, required: true },
+          answerOption: { type: String, required: true },
+          correct: { type: Boolean, required: true },
+          userAnswer: { type: Boolean, required: true },
+        },
+      ],
+    },
+  ],
+  __v: { type: Number, select: false },
+});
 
 const Quiz = mongoose.model("Quiz", quizSchema);
 
