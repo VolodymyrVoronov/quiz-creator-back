84390be98243055194464a16e774f89975b915af
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-26 15:30:33 +0100

    chore(quiz): hid __v for any responses.

--- models/quiz.js
+++ models/quiz.js
@@ -1,25 +1,28 @@
 import mongoose from "mongoose";
 
-const quizSchema = mongoose.Schema({
-  id: { type: String, required: true },
-  quizTitle: { type: String, required: true },
-  userId: { type: String, required: true },
+const quizSchema = mongoose.Schema(
+  { __v: { type: Number, select: false } },
+  {
+    id: { type: String, required: true },
+    quizTitle: { type: String, required: true },
+    userId: { type: String, required: true },
 
-  questions: [
-    {
-      id: { type: String, required: true },
-      question: { type: String, required: true },
-      options: [
-        {
-          id: { type: String, required: true },
-          answerOption: { type: String, required: true },
-          correct: { type: Boolean, required: true },
-          userAnswer: { type: Boolean, required: true },
-        },
-      ],
-    },
-  ],
-});
+    questions: [
+      {
+        id: { type: String, required: true },
+        question: { type: String, required: true },
+        options: [
+          {
+            id: { type: String, required: true },
+            answerOption: { type: String, required: true },
+            correct: { type: Boolean, required: true },
+            userAnswer: { type: Boolean, required: true },
+          },
+        ],
+      },
+    ],
+  }
+);
 
 const Quiz = mongoose.model("Quiz", quizSchema);
 
