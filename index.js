f922e98b5e80b0fb91cb307edd90813a2d80f8a0
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-27 11:31:30 +0100

    chore(index.js): added sanitizer and xss.

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoSanitize from "express-mongo-sanitize";
+import xss from "xss-clean";
 
 import { signup, signin } from "./utils/auth.js";
 
@@ -14,6 +16,9 @@ dotenv.config();
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.post("/signup", signup);
 app.post("/signin", signin);
