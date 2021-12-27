344db79d1eaa0e1ff6815e8c60e9ed402360c7ab
Volodymyr Voronov <malones.vladimir@gmail.com>
2021-12-27 11:39:43 +0100

    chore(index.js): installed helmet.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import mongoSanitize from "express-mongo-sanitize";
 import xss from "xss-clean";
+import helmet from "helmet";
 
 import { signup, signin } from "./utils/auth.js";
 
@@ -12,6 +13,8 @@ import quizRouter from "./routes/quiz.js";
 
 const app = express();
 dotenv.config();
+
+app.use(helmet());
 
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
